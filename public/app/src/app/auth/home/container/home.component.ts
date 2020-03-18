fc4890be633a17aa60f3ce86a083b9fb85aebe64
Earl Hans Genoso <earlhansg@gmail.com>
2020-03-19 01:53:19 +0800

    #3 Update Home Component and Module

--- public/app/src/app/auth/home/container/home.component.ts
+++ public/app/src/app/auth/home/container/home.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
 
+import { animations } from './home.animations';
+import { Subscription } from 'rxjs';
+
+/* Services */
+import { BreakPointsService } from '@app/shared/services';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html'
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  animations: [ animations.slideLeftOpacityTrigger ]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  constructor() {}
+  leftHeader: string;
+  private breakpointsSubcription$: Subscription;
+
+  constructor( private breakPointsService: BreakPointsService ) {}
+
+  ngOnInit() {
+    this.breakpointsSubcription$ = this.breakPointsService
+        .checkBreakPoints()
+        .subscribe((match: boolean) => {
+          if (match) {
+            this.leftHeader = 'hide';
+          } else { this.leftHeader = 'show'; }
+        });
+  }
+
+  ngOnDestroy() {
+    this.breakpointsSubcription$.unsubscribe();
+  }
+
 }
--- public/app/src/app/auth/home/home.module.ts
+++ public/app/src/app/auth/home/home.module.ts
@@ -5,16 +5,20 @@ import { SharedModule } from '@app/shared/shared.module';
 
 import { HomeRoutingModule } from '@app/auth/home/home-routing.module';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+
 import { HomeComponent } from './container/home.component';
-import { LoginSignUpComponent } from './components/login-signup/login-signup.component';
+import { LoginSignupComponent } from './components/login-signup/login-signup.component';
 
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    FlexLayoutModule
   ],
-  declarations: [ HomeComponent, LoginSignUpComponent ]
+  declarations: [ HomeComponent, LoginSignupComponent ]
 })
 export class HomeModule {}
