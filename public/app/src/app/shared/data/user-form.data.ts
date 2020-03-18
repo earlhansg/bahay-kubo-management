da3b5a601a2c825c0d0550e0343efb8a40dbc60e
Earl Hans Genoso <earlhansg@gmail.com>
2020-03-19 01:54:52 +0800

    #3 Provide data in FORM FIELDS

---
+++ public/app/src/app/shared/data/user-form.data.ts
@@ -0,0 +1,44 @@
+import { FormlyFieldConfig } from '@ngx-formly/core';
+
+
+/* SIGN IN FORM FIELDS */
+export const signInFormFields: FormlyFieldConfig[] = [
+  {
+    key: 'username',
+    type: 'input',
+    templateOptions: {
+      placeholder: 'Username',
+      required: true,
+    }
+  },
+  {
+    key: 'password',
+    type: 'input',
+    templateOptions: {
+      type: 'password',
+      placeholder: 'Password',
+      required: true,
+    }
+  }
+];
+
+/* SIGN UP FORM FIELDS */
+export const signUpFormFields: FormlyFieldConfig[] = [
+  {
+    key: 'username',
+    type: 'input',
+    templateOptions: {
+      placeholder: 'Username',
+      required: true,
+    }
+  },
+  {
+    key: 'password',
+    type: 'input',
+    templateOptions: {
+      type: 'password',
+      placeholder: 'Password',
+      required: true,
+    }
+  }
+];
