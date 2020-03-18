475d4f3562b4f90a5841a9cbd94cdab861db4fbd
Earl Hans Genoso <earlhansg@gmail.com>
2020-03-19 04:44:59 +0800

    #4 Created Condominium component

---
+++ public/app/src/app/condominium/container/condominium.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-condominium',
+  templateUrl: './condominium.component.html',
+  styleUrls: ['./condominium.component.scss']
+})
+export class CondominiumComponent {
+}
