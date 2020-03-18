01a3886c9b741ca8e72fcb966219783f37bd793e
Earl Hans Genoso <earlhansg@gmail.com>
2020-03-19 01:51:31 +0800

    #3 Login Signup UI

--- public/app/src/app/auth/home/components/login-signup/login-signup.component.ts
+++ public/app/src/app/auth/home/components/login-signup/login-signup.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ChangeDetectionStrategy } from '@angular/core';
 
+import { FormComponent } from '@app/shared/components/form/form.component';
 
+import { tabs, Tab } from './login-signup.data';
 @Component({
-  selector: 'app-login-signup',
-  templateUrl: './login-signup.component.html'
+selector: 'app-login-signup',
+changeDetection: ChangeDetectionStrategy.OnPush,
+templateUrl: './login-signup.component.html',
+styleUrls: ['./login-signup.component.scss']
 })
-export class LoginSignUpComponent {
+export class LoginSignupComponent implements OnInit {
+@ViewChild(FormComponent, {static: false}) formComponent: FormComponent;
 
-  constructor() {}
+tabs: Tab[] = tabs;
+
+constructor() { }
+
+ngOnInit() {}
+
+selectTab(index: number): void {
+  this.formComponent.form.reset();
+}
+
+onSubmittedForm(credentials): void {
+  const { username, password } = credentials;
+  const payload = { username, password };
+}
+
 }
