9192b20306eb5d728956035d533ca47cbe85f943
Earl Hans Genoso <earlhansg@gmail.com>
2020-03-20 00:25:06 +0800

    #5 Dashboard UI

---
+++ public/app/src/app/dashboard/container/dashboard.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+/* Data */
+import { navs, Nav } from './dashboard.data';
+import { Subscription } from 'rxjs';
+
+/* Services */
+import { BreakPointsService } from '@app/shared/services';
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit, OnDestroy {
+
+  navs: Nav[] = navs;
+  private breakpointsSubcription$: Subscription;
+  isSmallScreen: boolean;
+
+  constructor( private breakPointsService: BreakPointsService ) { }
+
+  ngOnInit() {
+    this.breakpointsSubcription$ = this.breakPointsService
+        .checkBreakPoints(`(max-width: 901px)`)
+        .subscribe((match: boolean) => this.isSmallScreen = match);
+  }
+
+  ngOnDestroy() {
+    this.breakpointsSubcription$.unsubscribe();
+  }
+
+  get sidenavMode() {
+    return this.isSmallScreen ? 'over' : 'side';
+  }
+
+}
