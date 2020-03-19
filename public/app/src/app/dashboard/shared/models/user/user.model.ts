e596d49c01c1f3c410273a6550a4bd39353b2706
Earl Hans Genoso <earlhansg@gmail.com>
2020-03-20 00:30:56 +0800

    #5 Created interface for Users

---
+++ public/app/src/app/dashboard/shared/models/user/user.model.ts
@@ -0,0 +1,24 @@
+export interface Credentials {
+    id: number;
+    username: string;
+    firstName: string;
+    lastName: string;
+    position: string;
+}
+
+export interface ProfilePicture {
+    caption: string;
+    url: string;
+}
+
+export class User {
+    id?: number;
+    username?: string;
+    password?: string;
+    firstName?: string;
+    lastName?: string;
+    position?: string;
+    profilePicture?: ProfilePicture;
+}
+
+export type UserProfile = Credentials & ProfilePicture;
