8e595abdffb84141e0636a7ee091360bc0acccc5
Earl Hans Genoso <earlhansg@gmail.com>
2020-03-20 00:30:19 +0800

    #5 User Profile Component's data

---
+++ public/app/src/app/dashboard/shared/components/user-profile/user-profile.data.ts
@@ -0,0 +1,12 @@
+import { UserProfile } from '@app/dashboard/shared/models';
+
+
+export const user: UserProfile = {
+    id: 1,
+    username: 'earlhansg',
+    firstName: 'Earl Hans',
+    lastName: 'Genoso',
+    position: 'Admin',
+    caption: 'MeMyselfandI',
+    url: 'assets/images/profile/me.jpg'
+};
