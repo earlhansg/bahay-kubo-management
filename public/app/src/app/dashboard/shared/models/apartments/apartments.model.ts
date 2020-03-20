e21ca67e6a15b8bd96ea77728b599eded042ba41
Earl Hans Genoso <earlhansg@gmail.com>
2020-03-20 20:03:01 +0800

    #7 Create interface for apartments

---
+++ public/app/src/app/dashboard/shared/models/apartments/apartments.model.ts
@@ -0,0 +1,9 @@
+
+export class Apartments {
+    apartmentId: number;
+    condoId: number;
+    apartmentUnitPrice: number;
+    apartmentNumberRooms: number;
+    apartmentSquareMeters: number;
+    apartmentAvailable: boolean;
+}
